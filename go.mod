b362e1d9086933212e09ec615f9ff3f02262ebb8
smf8 <mr.smf8@gmail.com>
2020-08-09 03:17:04 +0430

    feat: Add subscriber

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/pion/ion-sfu v0.0.0-20200807155507-abb544eab216
+	github.com/pion/rtcp v1.2.3
 	github.com/pion/webrtc/v3 v3.0.0-beta.1
 	github.com/sirupsen/logrus v1.2.0
 	github.com/smf8/producer v0.0.0-20200808120452-88fcbb8b60c3
---
+++ subscriber/subscriber.go
@@ -0,0 +1,61 @@
+package subscriber
+
+import (
+	"log"
+	"time"
+
+	"github.com/pion/rtcp"
+	"github.com/pion/webrtc/v3"
+	"github.com/sirupsen/logrus"
+	"github.com/smf8/sfu-load-test/client"
+)
+
+const PLICycle = 3 * time.Second
+
+type Subscriber interface {
+	Subscribe()
+}
+
+type ClientSubscriber struct {
+	client *client.Client
+}
+
+func NewClientSubscriber(client *client.Client) *ClientSubscriber {
+	// a media engine with default codecs was added implicitly
+	// Allow us to receive 1 audio track, and 1 video track
+	if _, err := client.Pc.AddTransceiverFromKind(webrtc.RTPCodecTypeVideo, webrtc.RtpTransceiverInit{
+		Direction: webrtc.RTPTransceiverDirectionRecvonly,
+	}); err != nil {
+		panic(err)
+	}
+
+	return &ClientSubscriber{client}
+}
+
+func (cs ClientSubscriber) Subscribe() {
+	cs.client.Pc.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+		logrus.Printf("Client Connection State has changed %s \n", connectionState.String())
+
+		if connectionState == webrtc.ICEConnectionStateClosed {
+			logrus.Println("Finished writing to video")
+		}
+	})
+
+	cs.client.Pc.OnTrack(func(track *webrtc.Track, receiver *webrtc.RTPReceiver) {
+		go func() {
+			ticker := time.NewTicker(PLICycle)
+			for range ticker.C {
+				errSend := cs.client.Pc.WriteRTCP([]rtcp.Packet{&rtcp.PictureLossIndication{MediaSSRC: track.SSRC()}})
+				if errSend != nil {
+					log.Println(errSend)
+				}
+			}
+		}()
+
+		cs.handleTrack(track)
+	})
+}
+
+func (cs *ClientSubscriber) handleTrack(tr *webrtc.Track) {
+
+}
